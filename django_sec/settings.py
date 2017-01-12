5869c95589133d15d1ef257b5bdd0ae997a484b4
Mads <madslundgaard@users.noreply.github.com>
2017-01-12 21:21:17 +0100

    Update settings.py

--- django_sec/settings.py
+++ django_sec/settings.py
@@ -9,4 +9,7 @@ DEFAULT_FILE_STORAGE = 'sec_http_storage.FallbackStorage'
 
 FALLBACK_STORAGE_URL = 'http://www.sec.gov/Archives'
 
-MEDIA_ROOT = '/sec_files/'
+MEDIA_ROOT = settings.django_sec_MEDIA_ROOT = getattr(
+    settings,
+    'django_sec_MEDIA_ROOT',
+    '/tmp/sec_files')
