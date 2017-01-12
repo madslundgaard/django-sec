c545517bf0e69455b52e130e3071e64c0bec36ea
Mads <madslundgaard@users.noreply.github.com>
2017-01-12 20:53:47 +0100

    Update settings to accommodate new storage
    Added settings from matthiask/django-http-fallback-storage and to accommodate a django file storage approach

--- django_sec/settings.py
+++ django_sec/settings.py
@@ -4,3 +4,9 @@ DATA_DIR = settings.django_sec_DATA_DIR = getattr(
     settings,
     'django_sec_DATA_DIR',
     '/tmp/django_sec')
+
+DEFAULT_FILE_STORAGE = 'sec_http_storage.FallbackStorage'
+
+FALLBACK_STORAGE_URL = 'http://www.sec.gov/Archives'
+
+MEDIA_ROOT = '/sec_files/'
